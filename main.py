9a247f62a3e3aef0f96ea128a9275b875f91c2a4
Wiktor Lemański <s27439@pjwstk.edu.pl>
2023-01-25 16:48:20 +0100

    dodane contains

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print ("hello world")
+import sys
+
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+
+def save_to_file(file_path, data):
+    with open(file_path, "w") as file:
+        file.write(data)
+
+
+def contains(name):
+    plik = read_file("baza.txt")
+    if name in plik:
+        print(plik)
+
+
+def run_program(name, params):
+    print(params)
+    if params[0] == 'push':
+        pass
+    if params[0]=='contains':
+        contains(params[1])
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    run_program('PyCharm', sys.argv[1:])
+
+
+
+
